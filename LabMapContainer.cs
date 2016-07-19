b946b2920ac1aa061aab6fc7a5e01afd625d5194
oguretsss <hestheone30@gmail.com>
2016-07-19 18:46:56 +0300

    Create LabMapContainer class

---
+++ LabMapContainer.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class LabMapContainer
+    {
+        int columns, rows;
+        public int[,] dungeonMap;
+        public int[,] renderingMap;
+        public bool[,] fogOfWarMap;
+        public bool[,] walkableMap;
+        public List<DungeonRoom> rooms;
+
+        public LabMapContainer(int[,] dungeonMap, int[,] renderingMap, bool[,] fogOfWarMap, bool[,] walkableMap, List<DungeonRoom> rooms)
+        {
+            // TODO: Complete member initialization
+            this.dungeonMap = dungeonMap;
+            columns = dungeonMap.GetLength(0);
+            rows = dungeonMap.GetLength(1);
+            this.renderingMap = renderingMap;
+            this.fogOfWarMap = fogOfWarMap;
+            this.walkableMap = walkableMap;
+            this.rooms = rooms;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\n=============Start of the dungeon==================\n");
+            sb.Append(System.String.Format("Dungeon of {0} columns and {1} rows, containing {2} rooms.\n", columns, rows, rooms.Count));
+            sb.Append("Dungeon map:\n");
+            for (int x = 0; x < columns; x++)
+            {
+                sb.Append("\n");
+                for (int y = 0; y < rows; y++)
+                {
+                    switch (dungeonMap[x, y])
+                    {
+                        case DungeonGenerator.SECRET_DOOR_TILE:
+                            sb.Append("S ");
+                            break;
+                        case DungeonGenerator.RADIATION_TILE:
+                            sb.Append("R ");
+                            break;
+                        case DungeonGenerator.KEY_LOCKED_DOOR_TILE:
+                            sb.Append("K ");
+                            break;
+                        case DungeonGenerator.COLLAPSED_DOOR_TILE:
+                            sb.Append("C ");
+                            break;
+                        case DungeonGenerator.WALL_TILE:
+                        case DungeonGenerator.SURROUNDING_WALL_TILE:
+                            sb.Append("  ");
+                            break;
+                        case DungeonGenerator.VERTICAL_WALL_TILE:
+                        case DungeonGenerator.HORIZONTAL_WALL_TILE:
+                        case DungeonGenerator.CORNER_TILE:
+                            sb.Append(1 + " ");
+                            break;
+                        case DungeonGenerator.DOOR_TILE:
+                            sb.Append("= ");
+                            break;
+                        default:
+                            sb.Append(dungeonMap[x, y].ToString() + " ");
+                            break;
+                    }
+                }
+            }
+            sb.Append("\n=============End of the dungeon==================\n");
+            return sb.ToString();
+        }
+    }
+}
